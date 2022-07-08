5581ca7b11810b859d661a527e89f7a0afcfa67a
Nelson <nelsonkenmochi@gmail.com>
2022-07-08 10:11:08 -0400

    fix: add sudo to installation of chrome

--- scripts/install-google-chrome.sh
+++ scripts/install-google-chrome.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Google Chrome for running tests
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub -P /app/ | apt-key add -  \
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub -P /app/ | sudo apt-key add -  \
   && echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> \
     /etc/apt/sources.list.d/google-chrome.list \
   && apt-get -y update \
